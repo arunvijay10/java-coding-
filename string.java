66ca2b807595e61b1d76e15273f018b19f7f3dd7
Parrot Live user <user@localhost.localdomain>
2020-08-10 10:53:19 +0000

    String in java

---
+++ string.java
@@ -0,0 +1,9 @@
+public class string{
+public static void main(String args[])
+{
+String name = " Arun Vijay";
+String Job = "Programmer";
+String quote = "Coding is the way to the highway of a life";
+System.out.println("Name:"+name +"\n Job :"+Job+ "\n Fav Quote: " +quote);
+}
+}
