8340ba07b22dc247e64e673755a596ffe453d092
Parrot Live user <user@localhost.localdomain>
2020-08-10 11:30:29 +0000

    this is increment and decrement operation

---
+++ incre.java
@@ -0,0 +1,13 @@
+public class incre{
+public static void main(String args[])
+{
+int number1 = 45;
+number1++;
+System.out.println("This number is Increased: "+ number1);
+int decre=45;
+decre--;
+System.out.println("This number is decrease:" +decre);
+
+}
+}
+
