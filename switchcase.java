1abf0aefa7d8abfb4add4fa2f03a6e88ce595a2b
Parrot Live user <user@localhost.localdomain>
2020-08-10 11:18:09 +0000

    switch case

---
+++ switchcase.java
@@ -0,0 +1,15 @@
+public class switchcase{
+public static void main(String args[])
+{
+String Name ="Arun Vijay";
+switch(Name)
+{
+case "Arun Vijay" : System.out.println("This is What you are What finding");
+break;
+case "Unknown": System.out.println("This is What you are not finding"); 
+break;
+default : System.out.println("Finding...."); 
+}
+}
+}
+ 
