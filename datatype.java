84ec41204211aae683d4c7fe9eec3e7f6db8a93d
Parrot Live user <user@localhost.localdomain>
2020-08-10 10:36:14 +0000

    datatype

---
+++ datatype.java
@@ -0,0 +1,16 @@
+public  class datatype{
+public static void main(String args[])
+{
+float num1 = 4.5f;
+int num2 = 45;
+long text = 4544545;
+boolean  thisis=true;
+System.out.println(" ");
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(thisis);
+System.out.println(text);
+}
+}
+
+
