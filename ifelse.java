ed923533711ccd8665c0af3be61b6d395cc32b83
Parrot Live user <user@localhost.localdomain>
2020-08-10 11:00:16 +0000

    if else

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse{
+public static void main(String args[])
+{
+String name = "ArunVijay";
+
+if (name =="ArunVijay")
+{
+System.out.println("This is his Name");
+}
+else
+{
+System.out.println("This is his Not Name");
+}
+}
+}
+
