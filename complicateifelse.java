8fa4a3ab59da9abaa6cde8832b2d086eec61462e
Parrot Live user <user@localhost.localdomain>
2020-08-10 11:07:25 +0000

    ifelse elseif

---
+++ complicateifelse.java
@@ -0,0 +1,25 @@
+public class complicateifelse{
+public static void main(String args[])
+{
+int name=45;
+String Name="ArunVijay";
+
+if( name ==45)
+{
+System.out.println("This is the exact number");
+}
+else if(name ==56)
+{
+System.out.println("This is not  the exact number");
+}
+else if(Name == "ArunVijay")
+{
+System.out.println("Right Person");
+}
+else
+{
+System.out.println("Enter Valid Number's");
+}
+}
+}
+
